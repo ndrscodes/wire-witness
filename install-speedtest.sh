326301a187319942c1ea90e952fa90487c647c36
Andreas Schmidt <ndrscodes@gmail.com>
2026-05-11 18:45:23 +0200

    fix: accept amd64 as valid architecture

--- install-speedtest.sh
+++ install-speedtest.sh
@@ -6,6 +6,8 @@ if [ -z "$TARGETARCH" ]; then
     TARGETARCH="x86_64"
 elif [ "$TARGETARCH" = "arm64" ]; then
     TARGETARCH="aarch64"
+elif [ "$TARGETARCH" = "amd64" ]; then
+    TARGETARCH="x86_64"
 fi
 URL="https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-$TARGETARCH.tgz"
 
